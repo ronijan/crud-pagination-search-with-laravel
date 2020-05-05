370cf2336d6370c103ab7a11905fe16b7e59f2c3
hk1 <hk@14112>
2020-05-05 11:55:47 +0200

    table artikel and factory created

---
+++ database/migrations/2020_05_05_095017_create_artikel_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtikelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artikel', function (Blueprint $table) {
+            $table->id();
+            $table->string('teaserbild')->default(null);
+            $table->string('dachzeile');
+            $table->string('ueberschrift');
+            $table->string('teasertext');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artikel');
+    }
+}
