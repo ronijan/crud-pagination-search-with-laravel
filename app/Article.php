81ae76a5a403d71203f72ed70583653f895201da
ronijan <7ussein.5alil@gmail.com>
2020-05-06 13:34:35 +0200

    value of attributes changed

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Article extends Model
 {
@@ -22,5 +23,20 @@ class Article extends Model
     public function getUpdatedAtAttribute($date)
     {
         return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('Y-m-d');
+    }
+
+    public function getTeasertextAttribute($date)
+    {
+        return Str::limit($date, $limit = 30, $end = '...');
+    }
+
+    public function getUeberschriftAttribute($date)
+    {
+        return Str::limit($date, $limit = 15, $end = '...');
+    }
+
+    public function getTeaserbildAttribute($date)
+    {
+        return Str::limit($date, $limit = 15, $end = '...');
     }
 }
