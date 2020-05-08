f492fb610d679106d0156c20707edcce73035273
ronijan <7ussein.5alil@gmail.com>
2020-05-08 11:55:06 +0200

    img tag on show view added

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -7,16 +7,19 @@
 
     <div class="row">
         <div class="col-md-12">
-{{--            dachzeile--}}
+            {{-- dachzeile--}}
             <h2>{{$article->dachzeile}}</h2>
-{{--            Überschrift--}}
+            {{-- Überschrift--}}
             <h4>{{$article->ueberschrift}}</h4>
-{{--            Title Author --}}
+            {{-- Title Author --}}
             <p>{{$article->title}} {{$article->author}}</p>
-{{--            TeaseText--}}
+            {{--TeaseText--}}
             <p>{{$article->teasertext}}</p>
-{{--             <img src="{{$article->teaserbild}}" alt="test bild">--}}
+            {{--Image--}}
+             <img src="{{$article->teaserbild}}" class="mb-3" alt="test bild" width="200px" height="200px">
+            {{--Created at--}}
             <p><b>Created at:</b> {{$article->created_at}}</p>
+            {{--Updated at--}}
             <p><b>Updated at:</b> {{$article->updated_at}}</p>
         </div>
     </div>
