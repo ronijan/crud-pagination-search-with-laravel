b8b64bec0ddc6d5d18223a855d1e3a8e5fd40c6a
ronijan <7ussein.5alil@gmail.com>
2020-05-08 11:43:45 +0200

    param on action controller

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
 
 class ArticlesController extends Controller
 {
+    /**
+     * @return Application|Factory|View
+     */
     public function index()
     {
         $articles = Article::query()->orderBy('id', 'desc')->paginate(3);
@@ -15,11 +22,18 @@ class ArticlesController extends Controller
         return view('articles.index', compact('articles', $articles));
     }
 
+    /**
+     * @return Application|Factory|View
+     */
     public function create()
     {
         return view('articles.create');
     }
 
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
     public function store(Request $request)
     {
        $data = $request->validate([
@@ -36,18 +50,31 @@ class ArticlesController extends Controller
         return Redirect::to('articles');
     }
 
+    /**
+     * @param $id
+     * @return Application|Factory|View
+     */
     public function show($id)
     {
         $article = Article::query()->findOrFail($id);
         return view('articles.show')->with('article', $article);
     }
 
+    /**
+     * @param $id
+     * @return Application|Factory|View
+     */
     public function edit($id)
     {
         $article = Article::query()->findOrFail($id);
         return view('articles.edit')->with('article', $article);
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return RedirectResponse
+     */
     public function update(Request $request, $id)
     {
         $request->validate([
@@ -71,6 +98,10 @@ class ArticlesController extends Controller
         return Redirect::to('articles');
     }
 
+    /**
+     * @param $id
+     * @return RedirectResponse
+     */
     public function destroy($id)
     {
         Article::where('id',$id)->delete();
